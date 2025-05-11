29a70ac30572fc41e0a7ffb7c4caa4c011637f28
SolareFlame <zeldac332@gmail.com>
2025-05-11 17:34:55 +0200

    🐛 Wrong title fixing + navbar padding

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import Navbar from "./Navbar.jsx";
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Navbar />
+        <div className="pt-[200px]">
+            <App />
+        </div>
     </React.StrictMode>
 );
